14731dfe0f8d37bf5e3a58eaf7767670269b08bb
jamesbeedy <jamesbeedy@gmail.com>
2016-07-21 10:32:51 -0700

    consider suggestions and feedback from juju list

---
+++ lib/charms/layer/supervisorlib.py
@@ -0,0 +1,59 @@
+import os
+import subprocess as sp
+from charms.reactive import set_state
+from charmhelpers.core import host
+from charmhelpers.core.templating import render
+
+
+def start(appname):
+    """Start supervisor
+    """
+    sp.call(("supervisorctl start %s" % appname).split(), shell=False)
+
+
+def stop(appname):
+    """Stop supervisor
+    """
+    sp.call(("supervisorctl stop %s" % appname).split(), shell=False)
+
+
+def reread(appname):
+    """Reread supervisor conf
+    """
+    sp.call(("supervisorctl reread %s" % appname).split(), shell=False)
+
+
+def update(appname):
+    """Update supervisor
+    """
+    sp.call(("supervisorctl update %s" % appname).split(), shell=False)
+        
+
+def render_supervisor_conf(appname, ctxt={}):
+    """ Render /etc/supervisor/conf.d/{appname}.conf
+        and start the application supervisor process.
+
+
+        :param ctxt: dictionary of context variables to render in
+                     the template
+    """
+    spvsr_avail = "%s.supervisor.available" % self.appname
+    spvsr_conf = "/etc/supervisor/conf.d/%s.conf" % self.appname
+    spvsr_tmpl = "%s.spvsr.conf" % self.appname
+    if os.path.exists(spvsr_conf):
+        stop()
+        os.remove(spvsr_conf)
+    # Render supervisor conf
+    render(source=spvsr_tmpl,
+           target=spvsr_conf,
+           owner="root",
+           perms=0o644,
+           context=ctxt)
+    # Ensure supervisor is running
+    if not host.service_running("supervisor"):
+        host.service_start("supervisor")
+    # Reread supervisor .conf and start/restart process
+    reread()
+    update()
+    start()
+    set_state(spvsr_avail)
--- reactive/supervisor.py
+++ reactive/supervisor.py
@@ -1,21 +1,11 @@
 from charms.reactive import set_state
-from charms.reactive import when
 from charms.reactive import when_not
 from charmhelpers.core import hookenv
-from charmhelpers.fetch import apt_install
 
 
-@when_not('supervisor.installed')
-def install_supervisor():
-    '''Install supervisor
-    '''
-    hookenv.status_set('maintenance', 'installing Supervisor')
-    apt_install(['supervisor'])
-    set_state('supervisor.installed')
-
-
-@when('supervisor.installed')
-@when_not('supervisor.available')
+@when_not("supervisor.available")
 def supervisor_avail():
-    hookenv.status_set('active', 'Supervisor available')
-    set_state('supervisor.available')
+    """Set supervisor.available
+    """
+    hookenv.status_set("maintenance", "installing Supervisor")
+    set_state("supervisor.available")
